4950ecf8dc988ae2106074d344eb6a15aaf55219
Giridhar <giridharprasad2897@gmail.com>
2020-04-09 12:24:59 +0530

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,73 +1,64 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import RepoTable from './RepoTable';
+import { DataTableSkeleton, Pagination } from 'carbon-components-react';
 
 const RepoPage = () => {
-  const headers = [
-    {
-      key: 'name',
-      header: 'Name',
-    },
-    {
-      key: 'createdAt',
-      header: 'Created',
-    },
-    {
-      key: 'updatedAt',
-      header: 'Updated',
-    },
-    {
-      key: 'issueCount',
-      header: 'Open Issues',
-    },
-    {
-      key: 'stars',
-      header: 'Stars',
-    },
-    {
-      key: 'links',
-      header: 'Links',
-    },
-  ];
+  const [headers, setHeaders] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const rows = [
-    {
-      id: '1',
-      name: 'Repo 1',
-      createdAt: 'Date',
-      updatedAt: 'Date',
-      issueCount: '123',
-      stars: '456',
-      links: 'Links',
-    },
-    {
-      id: '2',
-      name: 'Repo 2',
-      createdAt: 'Date',
-      updatedAt: 'Date',
-      issueCount: '123',
-      stars: '456',
-      links: 'Links',
-    },
-    {
-      id: '3',
-      name: 'Repo 3',
-      createdAt: 'Date',
-      updatedAt: 'Date',
-      issueCount: '123',
-      stars: '456',
-      links: 'Links',
-    },
-  ];
+  const [totalItems, setTotalItems] = useState(0);
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
 
-  return (
-    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-      <div className="bx--row repo-page__r1">
-        <div className="bx--col-lg-16">
-          <RepoTable headers={headers} rows={rows} />
+  useEffect(() => {
+    //local json hosted using json-server for development purpose
+    fetch('http://localhost:3004/data')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setHeaders(data.headers);
+        setRows(data.rows);
+        setTotalItems(data.rows.length);
+        setLoading(false);
+      })
+      .catch(err => console.error(err));
+    return () => {};
+  }, []);
+
+  if (loading) {
+    return (
+      <DataTableSkeleton columnCount={6} rowCount={10} headers={headers} />
+    );
+  } else {
+    return (
+      <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+        <div className="bx--row repo-page__r1">
+          <div className="bx--col-lg-16">
+            <RepoTable
+              headers={headers}
+              rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+            />
+            <Pagination
+              totalItems={totalItems}
+              backwardText="Previous page"
+              forwardText="Next page"
+              pageSize={currentPageSize}
+              pageSizes={[5, 10, 15, 25]}
+              itemsPerPageText="Items per page"
+              onChange={({ page, pageSize }) => {
+                if (pageSize !== currentPageSize) {
+                  setCurrentPageSize(pageSize);
+                }
+                setFirstRowIndex(pageSize * (page - 1));
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default RepoPage;
